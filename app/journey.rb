28e8025751dd6d5b854c6b302b17fbb8a6ea95b5
tunkhine126 <tunkhine126@gmail.com>
2019-05-08 17:31:30 -0400

    cleaned journey

--- app/journey.rb
+++ app/journey.rb
@@ -61,41 +61,41 @@ class Journey
          ********************************************************
          Which one of these statements do you most resonate with?
          ********************************************************\n"
-      choices = [
-        'This is fun!',
-        "This is harder than I thought.",
-        "This is going to be a lot of work.",
-        "This sucks, I have no idea what I am doing.",
-        "#%@!!*!*!!!!",
-        "OK, but it still sucks.",
-        "Quick, let's call it a day and say we learned something.",
-        "Hey, I think I'm actually catching on!",
-        "WOW, look how far I've come!",
-        "This is one of the things I am most proud of."]
+      choices = {
+        'This is fun!' => 1,
+        "This is harder than I thought." => 2,
+        "This is going to be a lot of work." => 3,
+        "This sucks, I have no idea what I am doing." => 4,
+        "#%@!!*!*!!!!" => 5,
+        "OK, but it still sucks." => 6,
+        "Quick, let's call it a day and say we learned something." => 7,
+        "Hey, I think I'm actually catching on!" => 8,
+        "WOW, look how far I've come!" => 9,
+        "This is one of the things I am most proud of." => 10}
       inputs = $prompt.multi_select(message, choices)
 
       inputs.each {|input|
         print "\n"
         case input
-        when 'This is fun!'
+        when 1
           puts "'Just keep being true to yourself, if you're passionate about something go for it. Don't sacrifice anything, just have fun.'...Blake Lewis"
-        when 'This is harder than I thought.'
+        when 2
           puts "'Live and work but do not forget to play, to have fun in life and really enjoy it.'...Eileen Caddy"
-        when 'This is going to be a lot of work.'
+        when 3
           puts "'When I hear somebody say 'Life is hard', I am always tempted to ask 'Compared to what?'...Sydney Harris"
-        when 'This sucks, I have no idea what I am doing.'
+        when 4
           puts "'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.'...Pele"
-        when '#%@!!*!*!!!!'
+        when 5
           puts "'I couldn't tell you what I am going to do next 'cause I have no idea, but I am open to anything.'...Lenny Kravitz"
-        when 'OK, but it still sucks.'
+        when 6
           puts "'Drain the swamp.'...Donald Trump"
-        when "Quick, let's call it a day and say we learned something."
+        when 7
           puts "'Technology doesn't address everything - for example, air travel still sucks.'...Brad Feld"
-        when "Hey, I think I'm actually catching on!"
+        when 8
           puts "'I'm only going to get better and better. Not going to get worse.'...Jason Pierre-Paul"
-        when "WOW, look how far I've come!"
+        when 9
           puts "'We know we are on the right path. Our journey is not finished, but we have come a long way.'...Muhammadu Buhari"
-        when "This is one of the things I am most proud of."
+        when 10
           puts "'Honestly, it's the most amazing feeling in the world to be able to work on something that you're really proud of.'...Dania Ramirez"
         # else
         #   puts "Hmmm...not one of the choices. Please try again."
